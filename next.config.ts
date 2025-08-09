22d8ae27f61944c3158cd325101261e1041c618a
Rahul Mehra <rahul.mehra@tcgre.com>
2025-08-09 17:41:00 +0530

    chore/no-ref: gh-pages implemented to deploy on github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // Needed for static export
+  basePath: "/your-repo-name", // Replace with your GitHub repo name
+  assetPrefix: "/your-repo-name/", // Ensures assets load from correct path
 };
 
 export default nextConfig;
