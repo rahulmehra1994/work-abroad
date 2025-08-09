e974a14bccd3c2a19b7de7419db95ac6bcbe952b
Rahul Mehra <rahul.mehra@tcgre.com>
2025-08-09 19:22:52 +0530

    chore/no-ref: allow all remote images

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,18 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https', // or '*' in Next 15
+        hostname: '**',    // matches all domains
+      },
+      {
+        protocol: 'http',  // in case you need http also
+        hostname: '**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
